e8e0b23a4fdb75367bddf80c41bb31400b0c9099
bonustrack <fabien@bonustrack.co>
2021-04-01 23:30:34 +0700

    Bee.js

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@
 
 <script>
 import { mapActions } from 'vuex';
+import { Bee, Utils } from '@ethersphere/bee-js/src';
 
 export default {
   watch: {
@@ -32,8 +33,30 @@ export default {
       }
     }
   },
-  mounted() {
-    this.init();
+  async mounted() {
+    await this.init();
+
+    setTimeout(async () => {
+      const bee = new Bee('https://gateway.ethswarm.org/');
+
+      // const signer = '0x634fb5a872396d9693e5c9f9d7233cfa93f395c093371017ff44aa9ae6564cdd';
+      const signer = await Utils.Eth.makeEthereumWalletSigner(this.$auth.provider.value);
+      console.log(signer.address);
+
+      const socWriter = bee.makeSOCWriter(signer);
+      const identifier = '0000000000000000000000000000000000000000000000000000000000000000';
+      const data = new Uint8Array([1, 2, 3, 4, 5]);
+      try {
+        const response = await socWriter.upload(identifier, data);
+        console.log('Response', response);
+      } catch (e) {
+        console.log(e);
+      }
+
+      const fileHash = await bee.uploadData('Snapshot is great');
+      const data1 = await bee.downloadData(fileHash);
+      console.log(fileHash, data1.text()); // prints 'Bee is awesome!'
+    }, 3e3);
   },
   methods: {
     ...mapActions(['init'])
